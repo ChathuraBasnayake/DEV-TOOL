7c3582c321cdf786622cf1c6442847d41afa9066
Chathura Basnayake <chathurabasnayake2007@gmail.com>
2026-06-26 17:23:12 +0530

    feat: export shared types, constants, and utilities from index entry point

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,16 @@
+// Export Types
+export * from './types/canvas';
+export * from './types/aws';
+export * from './types/compiler';
+export * from './types/guardrails';
+export * from './types/project';
+export * from './types/api';
+
+// Export Constants
+export * from './constants/awsMetadata';
+export * from './constants/connectionRules';
+export * from './constants/colors';
+
+// Export Utilities
+export * from './utils/naming';
+export * from './utils/validation';
